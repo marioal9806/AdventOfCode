a9401d6bba7ba7f85505de153397776f98b6cc60
marioal9806 <marioal9806@hotmail.com>
2022-12-20 04:54:34 -0600

    Add sln for part 2 day 4

--- 2022/day4/sln.py
+++ 2022/day4/sln.py
@@ -20,6 +20,21 @@ def get_elves_range(line):
     return [int(limit) for limit in elf1_str.split("-")], [int(limit) for limit in elf2_str.split("-")]
 
 def range_contains(elf1, elf2):
+    # if any extreme is the same [5-7,7-9]
+    if elf1[0] == elf2[0] or elf1[0] == elf2[1]:
+        return True
+    if elf1[1] == elf2[0] or elf1[1] == elf2[1]:
+        return True
+
+    # if range 1 encloses left edge of range 2 (4-6,5-7)
+    if elf1[0] <= elf2[0] and elf2[0] <= elf1[1]:
+        return True
+
+    # if range 1 encloses right edge of range 2 (4-6,3-5)
+    if elf1[0] <= elf2[1] and elf2[1] <= elf1[1]:
+        return True
+    
+    # If fully enclosed (2-8,3-7)
     if elf1[0] <= elf2[0]:
         if elf1[1] >= elf2[1]:
             return True
@@ -33,11 +48,8 @@ if __name__ == "__main__":
             elf1, elf2 = get_elves_range(line)
             if range_contains(elf1, elf2):
                 count += 1
-                continue
             elif range_contains(elf2, elf1):
                 count += 1
-            else:
-                continue
         print(count)
         
                 
